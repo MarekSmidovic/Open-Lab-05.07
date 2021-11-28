f9bf15d5e21ade40ecdd28db8a10ca9258e091d4
smidovic <msmidovic@gmail.com>
2021-11-28 14:14:49 +0100

    najmensie cislo

--- Open-Lab-05.07/Numbers.cs
+++ Open-Lab-05.07/Numbers.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace Open_Lab_05._07
 {
@@ -6,7 +7,16 @@ namespace Open_Lab_05._07
     {
         public int[] RemoveSmallest(int[] nums)
         {
-            throw new NotImplementedException();
+            int najmensieCislo = 100;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] < najmensieCislo)
+                {
+                    najmensieCislo = nums[i];
+                }
+            }
+            nums = nums.Where(val => val != najmensieCislo).ToArray();
+            return nums;
         }
     }
 }
